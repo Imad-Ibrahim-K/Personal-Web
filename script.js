9d7195bec853c261e1978db2d7a5dc1fd191b6fb
Imad Ibrahim <imadibrahim164@gmail.com>
2023-01-03 20:29:42 +0530

    added scroll on navbar

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-$(function(){
-    var navbar = $('header-inner');
-    $(window)scroll()
-})
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500 ) {
+    document.getElementById("scrolldown").style.top = "0";
+  } else {
+    document.getElementById("scrolldown").style.top = "-75px";
+  }
+}
